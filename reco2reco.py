973fa280f10d65eaf7e96bc2a2d0afcf5c9c52ab
Marco Rovere <marco.rovere@cern.ch>
2016-02-10 12:26:34 +0100

    Add Run/LS/Event check on matching tracks and oriAlgo plots.

--- reco2reco.py
+++ reco2reco.py
@@ -14,7 +14,7 @@ label = None # "generalTracks"
 DELTA_R_CUT = None # 0.01
 DELTA_PT_OVER_PT_CUT = None # 0.1
 
-variable2index = ["order", "sample", "eta", "phi", "pt", "hits", "phits", "ndof", "chi2", "algo", "originalAlgo", "quality"]
+variable2index = ["order", "sample", "eta", "phi", "pt", "hits", "phits", "ndof", "chi2", "algo", "originalAlgo", "quality", "run", "ls", "event"]
 histos = {}
 
 def match(a, b):
@@ -92,16 +92,21 @@ def compareTwoReco(reference, new, histos, debug=1):
     hits = getIndexOf("hits")
     algo = getIndexOf("algo")
     orialgo = getIndexOf("originalAlgo")
+    run = getIndexOf("run")
+    ls = getIndexOf("ls")
+    event_number = getIndexOf("event")
     print " ".join("%10s" % k for k in variable2index)
     for original_index, original in enumerate(reference):
         # Fill in cumulative plots for the reference sample first
         histos['reference_hits_vs_algo'].Fill(original[algo], original[hits])
+        histos['reference_hits_vs_orialgo'].Fill(original[orialgo], original[hits])
         histos['reference_hits_vs_pt'].Fill(original[pt], original[hits])
         histos['den'].Fill(original[pt])
         histos['den_eta'].Fill(original[eta])
         histos['den_phi'].Fill(original[phi])
         histos['den_hits'].Fill(original[hits])
         histos['den_algo'].Fill(original[algo])
+        histos['den_orialgo'].Fill(original[orialgo])
 
         # Now start to look for a matching track in the comparison track collection
         window_depth = 400 # elements to span to look for best candidate
@@ -128,6 +133,9 @@ def compareTwoReco(reference, new, histos, debug=1):
             # track collection
             new_valid[iBest] = False
             original_valid[original_index] = False
+            assert original[run] == new[iBest][run], "run mismatch"
+            assert original[ls] == new[iBest][ls], "ls mismatch"
+            assert original[event_number] == new[iBest][event_number], "event mismatch"
             if debug:
                 print original_index
                 print original
@@ -138,12 +146,15 @@ def compareTwoReco(reference, new, histos, debug=1):
             histos['num_phi'].Fill(original[phi])
             histos['num_hits'].Fill(original[hits])
             histos['num_algo'].Fill(original[algo])
+            histos['num_orialgo'].Fill(original[orialgo])
             histos['fake_num'].Fill(new[iBest][pt])
             histos['fake_num_eta'].Fill(new[iBest][eta])
             histos['fake_num_phi'].Fill(new[iBest][phi])
             histos['fake_num_hits'].Fill(new[iBest][hits])
             histos['fake_num_algo'].Fill(new[iBest][algo])
+            histos['fake_num_orialgo'].Fill(new[iBest][orialgo])
             histos['comparison_algo_vs_reference_algo'].Fill(original[algo], new[iBest][algo])
+            histos['comparison_orialgo_vs_reference_orialgo'].Fill(original[orialgo], new[iBest][orialgo])
             histos['comparison_hits_vs_reference_hits'].Fill(original[hits], new[iBest][hits])
 
     # Let's try a recovery loop with somewhat lesser stringent cuts
@@ -183,13 +194,16 @@ def compareTwoReco(reference, new, histos, debug=1):
                 histos['num_phi'].Fill(original[phi])
                 histos['num_hits'].Fill(original[hits])
                 histos['num_algo'].Fill(original[algo])
+                histos['num_orialgo'].Fill(original[orialgo])
                 histos['fake_num'].Fill(new[iBest][pt])
                 histos['fake_num_eta'].Fill(new[iBest][eta])
                 histos['fake_num_hits'].Fill(new[iBest][hits])
                 histos['fake_num_algo'].Fill(new[iBest][algo])
+                histos['fake_num_orialgo'].Fill(new[iBest][orialgo])
                 histos['comparison_algo_vs_reference_algo'].Fill(original[algo], new[iBest][algo])
+                histos['comparison_orialgo_vs_reference_orialgo'].Fill(original[orialgo], new[iBest][orialgo])
                 histos['comparison_hits_vs_reference_hits'].Fill(original[hits], new[iBest][hits])
-            
+
 
     # These are the tracks in the reference track collection
     # that have *not* been associated to any track in the
@@ -200,6 +214,7 @@ def compareTwoReco(reference, new, histos, debug=1):
         print "**** Lost tracks **** %d" % len(reference_not_assigned)
     for j in reference_not_assigned:
             histos['lost_hits_vs_algo'].Fill(j[algo], j[hits])
+            histos['lost_hits_vs_orialgo'].Fill(j[orialgo], j[hits])
             histos['lost_hits_vs_pt'].Fill(j[pt], j[hits])
             histos['lost_eta'].Fill(j[eta])
             if debug:
@@ -211,12 +226,14 @@ def compareTwoReco(reference, new, histos, debug=1):
     for i, j in enumerate(new):
         # Fill in the cumulative plots related to tracks in the comparison track collection
         histos['comparison_hits_vs_algo'].Fill(j[algo], j[hits])
+        histos['comparison_hits_vs_orialgo'].Fill(j[orialgo], j[hits])
         histos['comparison_hits_vs_pt'].Fill(j[pt], j[hits])
         histos['fake_den'].Fill(j[pt])
         histos['fake_den_eta'].Fill(j[eta])
         histos['fake_den_phi'].Fill(j[phi])
         histos['fake_den_hits'].Fill(j[hits])
         histos['fake_den_algo'].Fill(j[algo])
+        histos['fake_den_orialgo'].Fill(j[orialgo])
 
     # These are the tracks in the comparison track collection
     # that have *not* been associated to any track in the
@@ -227,16 +244,17 @@ def compareTwoReco(reference, new, histos, debug=1):
         print "**** Fake tracks **** %d" % len(new_not_assigned)
     for j in new_not_assigned:
             histos['fake_hits_vs_algo'].Fill(j[algo], j[hits])
+            histos['fake_hits_vs_orialgo'].Fill(j[orialgo], j[hits])
             histos['fake_hits_vs_pt'].Fill(j[pt], j[hits])
             if debug:
                 print j
     if debug:
         print "**** End of Fake tracks ****"
-    
+
 def bookHistograms():
     from ROOT import gROOT, gStyle, TCanvas, TF1, TFile, TTree, gRandom, TH1F, TH2F
-    histos.setdefault('num', TH1F("numerator_pt", "numerator_pt", 100, 0., 200.))
-    histos.setdefault('den', TH1F("denominator_pt", "denominator_pt", 100, 0., 200.))
+    histos.setdefault('num', TH1F("numerator_pt", "numerator_pt", 200, 0., 100.))
+    histos.setdefault('den', TH1F("denominator_pt", "denominator_pt", 200, 0., 100.))
     histos.setdefault('num_eta', TH1F("numerator_eta", "numerator_eta", 25, -2.5, 2.5))
     histos.setdefault('den_eta', TH1F("denominator_eta", "denominator_eta", 25, -2.5, 2.5))
     histos.setdefault('num_phi', TH1F("numerator_phi", "numerator_phi", 25, -3.15, 3.15))
@@ -245,8 +263,10 @@ def bookHistograms():
     histos.setdefault('den_hits', TH1F("denominator_hits", "denominator_hits", 50, 0., 50.))
     histos.setdefault('num_algo', TH1F("numerator_algo", "numerator_algo", 12, -0.5, 11.5))
     histos.setdefault('den_algo', TH1F("denominator_algo", "denominator_algo", 12, -0.5, 11.5))
-    histos.setdefault('fake_num', TH1F("fake_numerator_pt", "fake_numerator_pt", 100, 0., 200.))
-    histos.setdefault('fake_den', TH1F("fake_denominator_pt", "fake_denominator_pt", 100, 0., 200.))
+    histos.setdefault('num_orialgo', TH1F("numerator_orialgo", "numerator_orialgo", 12, -0.5, 11.5))
+    histos.setdefault('den_orialgo', TH1F("denominator_orialgo", "denominator_orialgo", 12, -0.5, 11.5))
+    histos.setdefault('fake_num', TH1F("fake_numerator_pt", "fake_numerator_pt", 200, 0., 100.))
+    histos.setdefault('fake_den', TH1F("fake_denominator_pt", "fake_denominator_pt", 200, 0., 100.))
     histos.setdefault('fake_num_eta', TH1F("fake_numerator_eta", "fake_numerator_eta", 25, -2.5, 2.5))
     histos.setdefault('fake_den_eta', TH1F("fake_denominator_eta", "fake_denominator_eta", 25, -2.5, 2.5))
     histos.setdefault('fake_num_phi', TH1F("fake_numerator_phi", "fake_numerator_phi", 25, -3.15, 3.15))
@@ -255,9 +275,14 @@ def bookHistograms():
     histos.setdefault('fake_den_hits', TH1F("fake_denominator_hits", "fake_denominator_hits", 50, 0., 50.))
     histos.setdefault('fake_num_algo', TH1F("fake_numerator_algo", "fake_numerator_algo", 12, -0.5, 11.5))
     histos.setdefault('fake_den_algo', TH1F("fake_denominator_algo", "fake_denominator_algo", 12, -0.5, 11.5))
+    histos.setdefault('fake_num_orialgo', TH1F("fake_numerator_orialgo", "fake_numerator_orialgo", 12, -0.5, 11.5))
+    histos.setdefault('fake_den_orialgo', TH1F("fake_denominator_orialgo", "fake_denominator_orialgo", 12, -0.5, 11.5))
     histos.setdefault('comparison_algo_vs_reference_algo', TH2F("comparison_algo_vs_reference_algo",
                                                                 "comparison_algo_vs_reference_algo",
                                                                 12, -0.5, 11.5, 12, -0.5, 11.5))
+    histos.setdefault('comparison_orialgo_vs_reference_orialgo', TH2F("comparison_orialgo_vs_reference_orialgo",
+                                                                      "comparison_orialgo_vs_reference_orialgo",
+                                                                      12, -0.5, 11.5, 12, -0.5, 11.5))
     histos.setdefault('comparison_hits_vs_reference_hits', TH2F("comparison_hits_vs_reference_hits",
                                                                 "comparison_hits_vs_reference_hits",
                                                                 50, 0., 50., 50, 0., 50.))
@@ -273,18 +298,30 @@ def bookHistograms():
     histos.setdefault('lost_hits_vs_algo', TH2F("lost_hits_vs_algo",
                                                 "lost_hits_vs_algo",
                                                 12, -0.5, 11.5, 50, 0., 50.))
+    histos.setdefault('reference_hits_vs_orialgo', TH2F("reference_hits_vs_orialgo",
+                                                     "reference_hits_vs_orialgo",
+                                                     12, -0.5, 11.5, 50, 0., 50.))
+    histos.setdefault('comparison_hits_vs_orialgo', TH2F("comparison_hits_vs_orialgo",
+                                                      "comparison_hits_vs_orialgo",
+                                                      12, -0.5, 11.5, 50, 0., 50.))
+    histos.setdefault('fake_hits_vs_orialgo', TH2F("fake_hits_vs_orialgo",
+                                                "fake_hits_vs_orialgo",
+                                                12, -0.5, 11.5, 50, 0., 50.))
+    histos.setdefault('lost_hits_vs_orialgo', TH2F("lost_hits_vs_orialgo",
+                                                "lost_hits_vs_orialgo",
+                                                12, -0.5, 11.5, 50, 0., 50.))
     histos.setdefault('reference_hits_vs_pt', TH2F("reference_hits_vs_pt",
                                                    "reference_hits_vs_pt",
-                                                   100, 0., 200., 50, 0., 50.))
+                                                   200, 0., 100., 50, 0., 50.))
     histos.setdefault('comparison_hits_vs_pt', TH2F("comparison_hits_vs_pt",
                                                     "comparison_hits_vs_pt",
-                                                    100, 0., 200., 50, 0., 50.))
+                                                    200, 0., 100., 50, 0., 50.))
     histos.setdefault('fake_hits_vs_pt', TH2F("fake_hits_vs_pt",
                                               "fake_hits_vs_pt",
-                                              100, 0., 200., 50, 0., 50.))
+                                              200, 0., 100., 50, 0., 50.))
     histos.setdefault('lost_hits_vs_pt', TH2F("lost_hits_vs_pt",
                                               "lost_hits_vs_pt",
-                                              100, 0., 100., 50, 0., 50.))
+                                              200, 0., 100., 50, 0., 50.))
     histos.setdefault('lost_eta', TH1F("lost_eta", "lost_eta", 25, -2.5, 2.5))
 
     for h in histos.keys():
@@ -299,7 +336,7 @@ def fileExists(f):
     if not os.path.exists(f):
         print "File %s not found. Quitting.\n" % f
         sys.exit(1)
-    
+
 def initializeGlobals(args):
     from DataFormats.FWLite import Handle, Events
     from ROOT import gROOT, gStyle, TCanvas, TF1, TFile, TTree, gRandom, TH1F, TH2F
@@ -327,11 +364,35 @@ def runComparison(args):
     from DataFormats.FWLite import Handle, Events
     from ROOT import gROOT, gStyle, TCanvas, TF1, TFile, TTree, gRandom, TH1F, TH2F
     bookHistograms()
+
+    # Since we use Multithreading in processing files, we are not
+    # guaranteed that the order of events in the 2 files to compare is
+    # the same. For this reason, we loose time with a double loop at
+    # the very beginning in order to establish the link between the
+    # indices in the 2 input files.
+    index_reference = {}
+    index_new = {}
+    for i in range(0, eventsRef.size()):
+      eventsRef.to(i)
+      run = eventsRef.eventAuxiliary().run()
+      ls = eventsRef.eventAuxiliary().luminosityBlock()
+      event_number = eventsRef.eventAuxiliary().event()
+      index_reference.setdefault((run, ls, event_number), i)
+    for i in range(0, eventsNew.size()):
+      eventsNew.to(i)
+      run = eventsNew.eventAuxiliary().run()
+      ls = eventsNew.eventAuxiliary().luminosityBlock()
+      event_number = eventsNew.eventAuxiliary().event()
+      index_new.setdefault((run, ls, event_number), i)
+
     for i in range(0, eventsRef.size()):
     #for i in range(0, 2):
       a = eventsRef.to(i)
       print "Event", i
       a = eventsRef.getByLabel(label, tracksRef)
+      run = eventsRef.eventAuxiliary().run()
+      ls = eventsRef.eventAuxiliary().luminosityBlock()
+      event_number = eventsRef.eventAuxiliary().event()
       trValOri = []
       trValNew = []
       for track in tracksRef.product():
@@ -340,24 +401,33 @@ def runComparison(args):
     #   if (track.pt()<2) : continue
           if args.quality:
               if (track.quality(track.qualityByName(args.quality))) :
-                  trValOri.append((10*int(100*track.eta())+track.phi(), "ori", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity"))))
+                  trValOri.append((10*int(100*track.eta())+track.phi(), "ori", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity")), run, ls, event_number))
               else:
                   print 'Ignoring non-highquality track: ', 10*int(100*track.eta())+track.phi(), "ori", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity"))
           else:
-              trValOri.append((10*int(100*track.eta())+track.phi(), "ori", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity"))))
-      a = eventsNew.to(i)
+              trValOri.append((10*int(100*track.eta())+track.phi(), "ori", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity")), run, ls, event_number))
+      index = None
+      if (run, ls, event_number) in index_new.keys():
+          index = index_new[(run, ls, event_number)]
+      else:
+          print "Skipping missing event from comparison file."
+          continue
+      a = eventsNew.to(index)
       a = eventsNew.getByLabel(label, tracksNew)
+      run = eventsNew.eventAuxiliary().run()
+      ls = eventsNew.eventAuxiliary().luminosityBlock()
+      event_number = eventsNew.eventAuxiliary().event()
       for track in tracksNew.product():
           #   if (track.phi()<0) : continue
           #   if (track.eta()<0) : continue
     #      if (track.pt()<2) : continue
           if args.quality:
               if (track.quality(track.qualityByName(args.quality))) :
-                  trValNew.append((10*int(100*track.eta())+track.phi(), "new", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity"))))
+                  trValNew.append((10*int(100*track.eta())+track.phi(), "new", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity")), run, ls, event_number))
               else:
                   print 'Ignoring non-highquality track: ', 10*int(100*track.eta())+track.phi(), "new", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity"))
           else:
-              trValNew.append((10*int(100*track.eta())+track.phi(), "new", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity"))))
+              trValNew.append((10*int(100*track.eta())+track.phi(), "new", track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), track.ndof(), track.chi2(), track.algo()-4, track.originalAlgo()-4, track.quality(track.qualityByName("highPurity")), run, ls, event_number))
       a = trValOri.sort(key=lambda tr: tr[0])
       a = trValNew.sort(key=lambda tr: tr[0])
       compareTwoReco(trValOri, trValNew, histos)
@@ -391,6 +461,12 @@ def runComparison(args):
     computeEfficiency(histos['num_algo'], histos['den_algo'], histos['eff_algo'], computeFake=False)
     histos.setdefault('fake_algo', histos['fake_num_algo'].Clone("Fake")).SetTitle('Fake_algo')
     computeEfficiency(histos['fake_num_algo'], histos['fake_den_algo'], histos['fake_algo'], computeFake=True)
+
+    # Orialgo
+    histos.setdefault('eff_orialgo', histos['num_orialgo'].Clone("Efficiency_orialgo")).SetTitle('Efficiency_orialgo')
+    computeEfficiency(histos['num_orialgo'], histos['den_orialgo'], histos['eff_orialgo'], computeFake=False)
+    histos.setdefault('fake_orialgo', histos['fake_num_orialgo'].Clone("Fake")).SetTitle('Fake_orialgo')
+    computeEfficiency(histos['fake_num_orialgo'], histos['fake_den_orialgo'], histos['fake_orialgo'], computeFake=True)
 
 
 def writeHistograms(args):
