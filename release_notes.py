3c86719fa97c35484b4896bead935b6a4259f935
Marco Rovere <marco.rovere@cern.ch>
2017-10-05 10:35:38 +0200

    Add PR submission/merging date. Move to Fire.

--- release_notes.py
+++ release_notes.py
@@ -4,7 +4,16 @@ import json
 import StringIO
 import pycurl
 import re
+import fire
 from githubAPI import github_api_token
+
+RX_LINKS = re.compile('^Link: <(.*?)>; rel="next", <(.*?)>; rel="last"')
+RX_RELEASE = re.compile('CMSSW_(\d+)_(\d+)_(\d+)(_pre[0-9]+)*(_cand[0-9]+)*')
+RX_COMMIT = re.compile(".*\#(\d{0,5})( from.*)")
+RX_SINGLECOMMIT = re.compile(".*cmssw/pull/(\d{0,5})")
+RX_COMPARE = re.compile("(^https://github.*compare.*\.\.\..*)")
+
+DEBUG = True
 
 def head():
   head = """<!DOCTYPE html>
@@ -44,21 +53,48 @@ use our own local installation. -->
 """
   return trail
 
-def getReleasesNotes():
-  RX_LINKS = re.compile('^Link: <(.*?)>; rel="next", <(.*?)>; rel="last"')
-  RX_RELEASE = re.compile('CMSSW_(\d+)_(\d+)_(\d+)(_pre[0-9]+)*(_cand[0-9]+)*')
-  RX_COMMIT = re.compile(".*\#(\d{0,5})( from.*)")
-  RX_COMPARE = re.compile("(^https://github.*compare.*\.\.\..*)")
+def extractPRnumbers(release_notes):
+    prs = [re.match(RX_SINGLECOMMIT, l).group(1) for l in release_notes.split('\n') if re.match(RX_SINGLECOMMIT, l)]
+    if DEBUG:
+       for pr in prs:
+          print "PR: ", pr
+    return prs
+
+def checkRateLimits(header):
+    head = {}
+    hh = [h.split(':') for h in header.split('\n')]
+    for o in hh:
+        if len(o) > 1:
+            head.setdefault(o[0],o[1].replace('\r', ''))
+    if DEBUG:
+        print head
+    if int(head['X-RateLimit-Remaining']) == 0:
+        print "Rate Limits exceeded"
+        print "X-RateLimit-Limit", head['X-RateLimit-Limit']
+        print "X-RateLimit-Reset", head['X-RateLimit-Reset']
+        print "X-RateLimit-Remaining", head['X-RateLimit-Remaining']
+        import sys
+        sys.exit(1)
+
+def getReleasesNotes(selected_releases_regexp):
   next_link = 'first'
   last_link = 'last'
   current_link = "https://api.github.com/repos/cms-sw/cmssw/releases"
+  pr_current_link = "https://api.github.com/repos/cms-sw/cmssw/pulls"
   response = StringIO.StringIO()
+  pr_response = StringIO.StringIO()
   header   = StringIO.StringIO()
+  pr_header   = StringIO.StringIO()
   c = pycurl.Curl()
   c.setopt(pycurl.URL, current_link)
   c.setopt(pycurl.WRITEFUNCTION, response.write)
   c.setopt(pycurl.HEADERFUNCTION, header.write)
   c.setopt(pycurl.HTTPHEADER, ['Authorization: token %s' % github_api_token])
+  cpr = pycurl.Curl()
+  cpr.setopt(pycurl.URL, pr_current_link)
+  cpr.setopt(pycurl.WRITEFUNCTION, pr_response.write)
+  cpr.setopt(pycurl.WRITEFUNCTION, pr_response.write)
+  cpr.setopt(pycurl.HEADERFUNCTION, pr_header.write)
   counter = -1
   notes = []
 
@@ -67,6 +103,7 @@ def getReleasesNotes():
 
     releases = json.loads(response.getvalue())
     headers = header.getvalue()
+    checkRateLimits(headers)
 #    print releases, headers
     for line in headers.split('\n'):
        m = re.match(RX_LINKS, line)
@@ -79,11 +116,26 @@ def getReleasesNotes():
     for i in range(len(releases)):
       counter += 1
       rel_numbers = re.match(RX_RELEASE, releases[i]['name'])
-      if rel_numbers:
+      if rel_numbers and re.match(selected_releases_regexp, releases[i]['name']):
 #        print rel_numbers.groups()
 #        print releases[i]
         release_notes = re.sub(RX_COMMIT, '\\n1. [\\1](http://github.com/cms-sw/cmssw/pull/\\1) \\2', releases[i]['body'])
         release_notes = re.sub(RX_COMPARE, '\\1\n\n', release_notes)
+        if DEBUG:
+            print release_notes
+        prs = extractPRnumbers(release_notes)
+        for pr in prs:
+            cpr.setopt(pycurl.URL, pr_current_link + "/%s" % pr)
+            cpr.setopt(pycurl.HTTPHEADER, ['Authorization: token %s' % github_api_token])
+            cpr.perform()
+            checkRateLimits(pr_header.getvalue())
+            commit = json.loads(pr_response.getvalue())
+            if commit.has_key('created_at') and commit.has_key('merged_at'):
+                release_notes = re.sub('\[%s\](.*)' % pr, '[%s]\\1 created: %s merged: %s' % (pr, commit['created_at'], commit['merged_at']), release_notes)
+            pr_response.seek(0)
+            pr_response.truncate()
+            pr_header.seek(0)
+            pr_header.truncate()
 #        print release_notes
         notes.append([int(rel_numbers.group(1)),
                       int(rel_numbers.group(2)),
@@ -99,7 +151,7 @@ def getReleasesNotes():
     header.seek(0)
     header.truncate()
 
-  current = 0
+  current = -1
   out_rel = None
   notes = sorted(notes, key = lambda x: (x[0], x[1], x[2], x[3], x[4]), reverse=True)
   for r in notes:
@@ -107,6 +159,7 @@ def getReleasesNotes():
     if new_current != current:
       if out_rel:
         out_rel.write(trail())
+        out_rel.close()
       out_rel = open("ReleaseNotes_%s_%s.html" % (r[0], r[1]), "w")
       current = new_current
       out_rel.write(head())
@@ -114,7 +167,7 @@ def getReleasesNotes():
       out_rel.write('# %s\n%s' % (r[5].encode('ascii', 'replace'), r[6].encode('ascii', 'replace')))
     except:
       pass
-
+  out_rel.write(trail())
 
 if __name__ == '__main__':
-  getReleasesNotes()
+  fire.Fire(getReleasesNotes)
